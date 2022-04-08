fd052e250f73ec3b43512225eae397b189191736
Frithjof Klös <frithjof.kloes@shopgate.com>
2022-04-08 13:48:17 +0200

    CCP-2556 Fixed hiding of AddToCartBar when input is inside the bar

--- frontend/helpers/focusListener.js
+++ frontend/helpers/focusListener.js
@@ -8,19 +8,61 @@ import {
 
 let timeout;
 
+/* eslint-disable func-names */
+
+/**
+ * Polyfill for el.closest() function to find parent elements by selector
+ * https://developer.mozilla.org/en-US/docs/web/api/element/closest#polyfill
+ */
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector;
+}
+
+if (Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    let el = this;
+
+    do {
+      if (Element.prototype.matches.call(el, s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
+
+/* eslint-enable */
+
 /**
  * Checks if event relates to a keyboard show/hide events.
  * @param {MouseEvent} e Event.
  * @returns {boolean}
  */
 function eventRelatesToAKeyboard(e) {
-  return e.target.tagName === 'INPUT';
+  const fromInput = e.target.tagName === 'INPUT';
+
+  if (!fromInput) {
+    return false;
+  }
+
+  let fromTabBar = false;
+
+  try {
+    // Check if input was inside of the add-to-cart-bar - don't hide bars
+    fromTabBar = e.target.closest('.theme__product__add-to-cart-bar');
+  } catch (err) {
+    // nothing to do here
+  }
+
+  return !fromTabBar;
 }
+
 /**
  * Listener for `focusin` and `focusout` events which is showing/hiding all components
  * which could be visible in wrong places when keyboard is open.
  * @param {HtmlElement} document Html document
- * @param {function} dispatch Dispatch function.
+ * @param {Function} dispatch Dispatch function.
  */
 function focusListener(document, {
   dispatch,
